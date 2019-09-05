44371010afc0d21e5b7214429c73447bc0c9d23a
kaz91 <kaz9130n@gmail.com>
2019-09-05 21:33:19 +0900

    衝突処理
    ボールの移動
    ボールとブロックの衝突
    ブロックの消滅

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.GetComponent<Rigidbody2D>().AddForce(transform.up * speed);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Debug.Log("衝突");
+        Destroy(collision.gameObject);
+    }
+}
